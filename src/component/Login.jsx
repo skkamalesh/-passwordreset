822b2fac328045312560c1798b25202604009745
skkamalesh <kamalesh0326@gmail.com>
2023-11-09 14:29:03 +0530

    updates

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,120 +1,118 @@
-import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { toast } from 'react-toastify';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+import styled from "styled-components";
 
 const Login = () => {
-	const navigate = useNavigate();
-	const [formData, setFormData] = useState({ email: '', password: '' });
-	const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [loading, setLoading] = useState(false);
 
-	const handleChange = (e) => {
-		setFormData({ ...formData, [e.target.name]: e.target.value });
-	};
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		setLoading(true);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-		try {
-			// API call to login
-			const response = await axios.post(
-				`${import.meta.env.VITE_API_URL}/login`,
-				formData
-			);
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/login`,
+        formData
+      );
 
-			if (response.status === 200) {
-				toast.success(response.data.message, {
-					position: 'top-right',
-					autoClose: 3000,
-					hideProgressBar: false,
-					closeOnClick: true,
-					pauseOnHover: false,
-					draggable: true,
-					theme: 'light',
-				});
-			}
-		} catch (error) {
-			toast.error(error.response.data.message, {
-				position: 'top-right',
-				autoClose: 3000,
-				hideProgressBar: false,
-				closeOnClick: true,
-				pauseOnHover: false,
-				draggable: true,
-				theme: 'light',
-			});
-		} finally {
-			setLoading(false);
-		}
-	};
+      if (response.status === 200) {
+        toast.success(response.data.message, {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          theme: "light",
+        });
+      }
+    } catch (error) {
+      toast.error(error.response.data.message, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        theme: "light",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	return (
-		<Container>
-			<Wrapper>
-				<Form onSubmit={handleSubmit}>
-					<Form.Group controlId='email'>
-						<Form.Label>Email</Form.Label>
-						<Form.Control
-							type='email'
-							name='email'
-							value={formData.email}
-							onChange={handleChange}
-						/>
-					</Form.Group>
+  return (
+    <Container>
+      <Wrapper>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </Form.Group>
 
-					<Form.Group controlId='password'>
-						<Form.Label>Password</Form.Label>
-						<Form.Control
-							type='password'
-							name='password'
-							value={formData.password}
-							onChange={handleChange}
-						/>
-					</Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </Form.Group>
 
-					<Button
-						className='mt-2'
-						variant='primary'
-						type='submit'
-						disabled={loading}
-					>
-						Log In
-					</Button>
+          <Button
+            className="mt-2"
+            variant="primary"
+            type="submit"
+            disabled={loading}
+          >
+            Log In
+          </Button>
 
-					<Button
-						className='mt-2'
-						variant='secondary'
-						onClick={() => navigate('/create-user')}
-					>
-						Create User
-					</Button>
-				</Form>
-			</Wrapper>
-		</Container>
-	);
+          <Button
+            className="mt-2"
+            variant="secondary"
+            onClick={() => navigate("/create-user")}
+          >
+            Create User
+          </Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
 };
 
-// Styled components
 const Wrapper = styled.div`
-	width: 350px;
-	height: 250px;
-	background-color: #ffffff4f;
-	padding: 20px;
-	border-radius: 15px;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
+  width: 350px;
+  height: 250px;
+  background-color: #ffffff4f;
+  padding: 20px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const Container = styled.div`
-	width: 100vw;
-	height: 100vh;
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default Login;
